daf81ccacb3844af3999ad1c31a4d87bc1421d99
Grant Watson <gwatson117@gmail.com>
2021-05-22 20:43:16 -0600

    Update to Application User

--- Server/Models/ApplicationUser.cs
+++ Server/Models/ApplicationUser.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
-
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +10,17 @@ namespace ExpenseTracker.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
     }
 }

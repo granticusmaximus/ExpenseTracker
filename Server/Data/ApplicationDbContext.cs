98b97c3c69bf67bbf538cd46b94edaf0c373eb47
Grant Watson <gwatson117@gmail.com>
2021-05-22 20:29:29 -0600

    Initial commit

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using ExpenseTracker.Server.Models;
+
+using IdentityServer4.EntityFramework.Options;
+
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}

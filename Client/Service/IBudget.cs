07f4d546dc2350262b9ef4685fd8d549bada1fe3
Grant Watson <gwatson117@gmail.com>
2021-06-05 13:20:11 -0600

    Update to Interface of Service

--- Client/Service/IBudget.cs
+++ Client/Service/IBudget.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExpenseTracker.Shared.Models;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,18 +9,23 @@ namespace ExpenseTracker.Client.Service
 {
     public interface IBudget
     {
-        /*
-         * AddIncome
-         * UpdateIncome
-         * DeleteIncome
-         * SumeOfIncome
-         * 
-         * AddExpense
-         * UpdateExpense
-         * DeleteExpense
-         * SumOfExpense
-         * 
-         * AmountLeft{SumOfIncome - SumOfExpense}
-         */
+        #region Income CRUD
+        Task<List<Income>> LoadAllIncomeAsync();
+        Task<Income> AddIncome();
+        Task<Income> UpdateIncome();
+        Task<Income> DeleteIncome();
+        Task<Income> SumOfIncome();
+        #endregion
+
+
+        #region Expense CRUD
+        Task<List<Expense>> LoadAllExpensesAsync();
+        Task<Expense> AddExpense();
+        Task<Expense> UpdateExpense();
+        Task<Expense> DeleteExpense();
+        Task<Expense> SumOfExpense();
+        #endregion
+
+        /* AmountLeft{SumOfIncome - SumOfExpense}*/
     }
 }
